256e23aceb6912f062688130a248ed70b07a29ec
Oleksii <achili3f17@gmail.com>
2022-10-30 21:19:56 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Notification } from 'components/Notification/Notification';
 import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
 import { Statistics } from '../Statistics/Statistics';
 import { Section } from '../Section/Section';
-import { Notification } from 'components/Notification/Notification';
 import { Wrapper } from './App.styled';
 
 export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const allState = { good, neutral, bad };
+  const [total, setTotal] = useState(0);
+  const [positivePercentage, setPositivePercentage] = useState(0);
 
-  const total = good + neutral + bad
- 
+  const allFeedBack = { good, neutral, bad };
 
-  const countPositiveFeedbackPercentage = () => {
-    if (!total) {
-      return 0;
-    }
-    const result = (good / total) * 100;
-    return Number(result.toFixed(0));
-  };
 
-  const positivePercentage = countPositiveFeedbackPercentage();
 
   const handleIncrement = event => {
     const name = event.target.name;
@@ -37,11 +29,19 @@ export default function App() {
     }
   };
 
+useEffect(() => {
+    const allState = good + neutral + bad;
+    setTotal(allState);
+    setPositivePercentage(Math.round((100 / allState) * good) || 0);
+}, [good, neutral, bad]);
+  
+  
+
   return (
     <Wrapper>
       <Section title="Please leave feedback">
         <FeedbackOptions
-          options={Object.keys(allState)}
+          options={Object.keys(allFeedBack)}
           onLeaveFeedback={handleIncrement}
         />
       </Section>
